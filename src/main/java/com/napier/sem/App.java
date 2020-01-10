11b24fb8775e34e2825657ae36e136f6ae463725
40437522 <40437522@live.napier.ac.uk>
2020-01-10 12:02:22 +0630

    Some changes

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,17 +9,18 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+//        MongoClient mongoClient = new MongoClient("localhost", 27000);
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Chalmers")
+        Document doc = new Document("name", "DevOps-Gp4")
                 .append("class", "Software Engineering Methods")
-                .append("year", "2018/19")
-                .append("result", new Document("CW", 95).append("EX", 85));
+                .append("year", "2019/2020");
         // Add document to collection
         collection.insertOne(doc);
 
