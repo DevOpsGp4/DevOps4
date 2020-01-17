b0311770cb0f6d2965dbee75c62db3a680014987
Khant Sithu Aung <45054709+caster082@users.noreply.github.com>
2020-01-17 09:49:02 +0630

    Adding comments(Code quality)@app

--- src/main/java/com/napier/DevOps4/App.java
+++ src/main/java/com/napier/DevOps4/App.java
@@ -13,21 +13,22 @@ public class App
         }
         catch (ClassNotFoundException e)
         {
+            // If Class not found exception,DB driver loading fail
             System.out.println("Could not load SQL driver");
-            System.exit(-1);
+            System.exit(-1); // exit
         }
 
         // Connection to the database
         Connection con = null;
         int retries = 100;
-        for (int i = 0; i < retries; ++i)
+        for (int i = 0; i < retries; ++i)// Trying to connect database by looping
         {
             System.out.println("Connecting to database...");
             try
             {
-                // Wait a bit for db to start
+                // Waiting stage to startup database
                 Thread.sleep(30000);
-                // Connect to database
+                // Connecting to database by user root and password example
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 // Wait a bit
@@ -36,17 +37,18 @@ public class App
                 break;
             }
             catch (SQLException sqle)
-            {
+            {   //if thr is no exception about connecting DB,attempt fail error
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
             }
             catch (InterruptedException ie)
             {
+                //If there something interrupted,print out
                 System.out.println("Thread interrupted? Should not happen.");
             }
-        }
+        }//Exit loop
 
-        if (con != null)
+        if (con != null)//if conection is null,make loop to to close DB
         {
             try
             {
