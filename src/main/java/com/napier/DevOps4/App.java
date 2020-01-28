9a0164adc489fd03e371dc60c4cf09e496a8e3f5
HanTun-dev <59551377+hantun-dev@users.noreply.github.com>
2020-01-28 13:48:55 +0630

    Top N capital cities

--- src/main/java/com/napier/DevOps4/App.java
+++ src/main/java/com/napier/DevOps4/App.java
@@ -55,6 +55,7 @@ public class App
             }
             if (n == 3) {
                 // capital city report
+                subMenu3();
             }
             if (n == 4) {
                 // population report
@@ -238,12 +239,12 @@ public class App
      * @param cities The list of countries to display.
      */
     public void displayCities (ArrayList < city > cities) {
-        // Check employees is not null
-//        if (cities == null)
-//        {
-//            System.out.println("No countries");
-//            return;
-//        }
+        // Check search data is not null
+        if (cities == null)
+        {
+            System.out.println("No countries");
+            return;
+        }
         for (city cy : cities) {
             System.out.format("%1$-20s %2$-25s %3$-25s %4$-20s \n", cy.getName(),cy.getCountryCode(),cy.getDistrict(),cy.getPopulation());
         }
@@ -530,4 +531,210 @@ public class App
         }
     }
 
+    /*Sub Menus3*/
+    public static void subMenu3() {
+        Scanner console = new Scanner(System.in);
+        // Create new Application
+        App a = new App();
+        // Connect to database
+        a.connect("localhost:33060");
+        char c;
+        int n = 0;
+
+        // this will be the sub menu that gets displayed.
+        System.out.println("  Capital City Report ");
+        System.out.println("===============================================");
+        System.out.println("1. All the capital cities in the world organised by largest population to smallest.");
+        System.out.println("2. All the capital cities in a continent organised by largest population to smallest.");
+        System.out.println("3. All the capital cities in a region organised by largest to smallest.");
+        System.out.println("4. The top N populated capital cities in the world where N is provided by the user.");
+        System.out.println("5. The top N populated capital cities in a continent where N is provided by the user.");
+        System.out.println("6. The top N populated capital cities in a region where N is provided by the user.");
+        System.out.println("===============================================");
+        System.out.println("7. EXIT SUB MENU");
+
+        while (n != 7)// Exits the program when 5 is pressed
+        {
+            System.out.print("\n Please enter option 1-6 to continue or 7 to exit...: ");
+            n = Integer.parseInt(System.console().readLine());
+            // Reads user input and takes them to selected code.
+            if (n > 7 || n < 1) {
+                //Invalid input
+                System.out.print("Invalid input, please try again...");
+            }
+            if (n == 7){
+                // Exit output
+                System.out.print("Back to Menus...");
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                Menus();
+            }
+            /*if (n == 1)
+            {
+                ArrayList<city> cities = a.queryCC1();
+                a.displayCCities(cities);
+                continue;
+            }
+
+            if (n == 2)
+            {
+                ArrayList<city> cities = a.queryCC2();
+                a.displayCCities(cities);
+                continue;
+            }
+            if (n == 3)
+            {
+                ArrayList<city> cities = a.queryCC3();
+                a.displayCCities(cities);
+                continue;
+            }*/
+            if (n == 4)
+            {
+                ArrayList<city> Ccities = a.queryCC4();
+                a.displayCCities(Ccities);
+                continue;
+            }
+            if (n == 5)
+            {
+                ArrayList<city> Ccities = a.queryCC5();
+                a.displayCCities(Ccities);
+                continue;
+            }
+            if (n == 6)
+            {
+                ArrayList<city> Ccities = a.queryCC6();
+                a.displayCCities(Ccities);
+                continue;
+            }
+        }
+    }
+    /**
+     * Set Methods
+     * @return An array list of all countries
+     */
+    public ArrayList<city> getCcities  (Statement stmt, String strSelect) throws SQLException {
+        ResultSet resultSet = stmt.executeQuery(strSelect);
+        // Extract country information
+        ArrayList<city> Ccities = new ArrayList<city>();
+        while (resultSet.next()) {
+            city ccy = new city();
+            ccy.setName(resultSet.getString(1));
+            ccy.setCountryCode(resultSet.getString(2));
+            ccy.setPopulation(resultSet.getInt(3));
+            Ccities.add(ccy);
+        }
+        return Ccities;
+    }
+    /**
+     * Display a list of Capital Cities.
+     //* @param Capital Cities The list of countries to display.
+     */
+    public void displayCCities (ArrayList < city > Ccities) {
+        //Check cities is not null
+        if (Ccities == null)
+        {
+            System.out.println("No Capital Cities");
+            return;
+        }
+        for (city ccy : Ccities) {
+            if (ccy == null)
+                continue;
+            System.out.format("%1$-20s %2$-25s %3$-25s \n", ccy.getName(),ccy.getCountryCode(),ccy.getPopulation());
+        }
+    }
+
+    /**
+     * Get The top N populated capital cities in a district where N is provided by the user.
+     * @return A list of capital cities and population, or null if there is an error.
+     */
+    public ArrayList<city> queryCC4 () {
+        System.out.println("4 - The top N populated capital cities in the world where N is provided by the user\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount of cities - ");
+        int limit = scanner.nextInt();
+
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect = "SELECT city.Name, country.Name,city.Population from city, country WHERE city.CountryCode=country.Code ORDER BY city.Population DESC LIMIT "+limit;
+
+            // Execute SQL statement
+            return getCcities(stmt, strSelect);
+        } catch (Exception e) //Catch any errors and print error message
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+    }
+
+    /**
+     * Get theThe top N populated cities in a continent where N is provided by the user.
+     * @return A list of all cities and population, or null if there is an error.
+     */
+    public ArrayList<city> queryCC5 () {
+        System.out.println("5 - The top N populated cities in a continent where N is provided by the user.\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount of  Cities - ");
+        int input = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the Name of continent- ");
+        String Ncon  = scanner.nextLine();
+
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+
+            // Create string for SQL statement
+            String limit = "LIMIT " + input + " ";
+
+            String strSelect =
+                    "SELECT city.Name, country.Name,city.Population from city, country WHERE city.CountryCode=country.Code AND country.Continent='"+Ncon+"' ORDER BY city.Population DESC  "+limit;
+
+            // Execute SQL statement
+            return getCcities(stmt, strSelect);
+        } catch (Exception e) //Catch any errors and print error message
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+    }
+
+    /**
+     * Get The top N populated cities in a region where N is provided by the user.
+     * @return A list of all cities and population, or null if there is an error.
+     */
+    public ArrayList<city> queryCC6 () {
+        System.out.println("6 - The top N populated cities in a region where N is provided by the user.\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount of cities - ");
+        int input = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter the name of region - ");
+        String Nreg  = scanner.nextLine();
+
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String limit = "LIMIT " + input + " ";
+            String strSelect =
+                    "SELECT city.Name, country.Name,city.Population from city, country WHERE city.CountryCode=country.Code AND country.region='"+Nreg+"' ORDER BY Population DESC   "+limit;
+
+            // Execute SQL statement
+            return getCcities(stmt, strSelect);
+        } catch (Exception e) //Catch any errors and print error message
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get details");
+            return null;
+        }
+    }
 }
