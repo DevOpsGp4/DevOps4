abedc818a2d5e3cc5119b73da4b4758ce7d9d8e1
KyawZawLynn <59551424+kyawzawlynn@users.noreply.github.com>
2020-02-07 18:20:12 +0630

    Testing

--- src/test/java/com/napier/DevOps4/AppIntegrationTest.java
+++ src/test/java/com/napier/DevOps4/AppIntegrationTest.java
@@ -40,5 +40,249 @@ public class AppIntegrationTest {
         Countrylanguage.CountryCode = "ABW";
         Countrylanguage.Language = "Dutch";
     }
+    //    @Test
+//    void testCountryWorld()
+//    {
+//        try{
+//            ArrayList<Country> countries = app.countryWorld();
+//            for(Country country : countries){
+//                country.Continent= "Asia";
+//                country.Name = "China";
+//                country.Code = "CHN";
+//                country.Capital= "1891";
+//                country.Population = 1277558000;
+//                country.Region = "Eastern Asia";
+//
+//            }
+//            System.out.print("Country world passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    void testCountryContinent()
+//    {
+//        try{
+//            ArrayList<Country> countries = app.countryContinent();
+//            for(Country country : countries){
+//                country.Continent= "Asia";
+//                country.Name = "China";
+//                country.Code = "CHN";
+//                country.Capital= "1891";
+//                country.Population = 1277558000;
+//                country.Region = "Eastern Asia";
+//
+//            }
+//            System.out.print("Country Continent passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    void testCountryRegion()
+//    {
+//        try{
+//            ArrayList<Country> countries = app.countryRegion();
+//            for(Country country : countries){
+//                country.Continent= "Asia";
+//                country.Name = "China";
+//                country.Code = "CHN";
+//                country.Capital= "1891";
+//                country.Population = 1277558000;
+//                country.Region = "Eastern Asia";
+//            }
+//            System.out.print("Country Region Passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    void testTopWorldCountry()
+//    {
+//        try{
+//            ArrayList<Country> countries = app.topWorldCountry();
+//            for(Country country : countries){
+//                country.Continent= "Asia";
+//                country.Name = "China";
+//                country.Code = "CHN";
+//                country.Capital= "1891";
+//                country.Population = 1277558000;
+//                country.Region = "Eastern Asia";
+//
+//            }
+//            System.out.print("Top World Country Passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    void testTopContinentCountry()
+//    {
+//        try{
+//            ArrayList<Country> countries = app.topContinentCountry();
+//            for(Country country : countries){
+//                country.Continent= "Asia";
+//                country.Name = "China";
+//                country.Code = "CHN";
+//                country.Capital= "1891";
+//                country.Population = 1277558000;
+//                country.Region = "Eastern Asia";
+//            }
+//            System.out.print("Top Country Continent Passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    void testTopRegionCountry()
+//    {
+//        try{
+//            ArrayList<Country> countries = app.topRegionCountry();
+//            for(Country country : countries){
+//                country.Continent= "Asia";
+//                country.Name = "China";
+//                country.Code = "CHN";
+//                country.Capital= "1891";
+//                country.Population = 1277558000;
+//                country.Region = "Eastern Asia";
+//            }
+//            System.out.print("Top Country Region Passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+    //    @Test
+//    void getPeopleCitiesContinentTest()
+//    {
+//        try{
+//            ArrayList<PopulationPercent> populationPercents = app.query23();
+//            for(PopulationPercent populationPercent : populationPercents){
+//                populationPercent.continentName = "Oceania";
+//                populationPercent.countryPopulation = Long.valueOf(30385150);
+//                populationPercent.liveInCity = Long.valueOf(13886149);
+//                populationPercent.liveInCityPercent = (float)45.7004;
+//                populationPercent.noLiveInCity = Long.valueOf(16499001);
+//                populationPercent.noLiveInCityPercent = (float)54.2996;
+//            }
+//            System.out.print("Population Percent check for Continent passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPeopleCitiesRegionTest()
+//    {
+//        try{
+//            ArrayList<PopulationPercent> populationPercents = app.query24();
+//            for(PopulationPercent populationPercent : populationPercents){
+//                populationPercent.regionName = "Micronesia";
+//                populationPercent.countryPopulation = Long.valueOf(543000);
+//                populationPercent.liveInCity = Long.valueOf(102329);
+//                populationPercent.liveInCityPercent = (float)18.8451;
+//                populationPercent.noLiveInCity = Long.valueOf(440671);
+//                populationPercent.noLiveInCityPercent = (float)81.1549;
+//            }
+//            System.out.print("Population Percent check for Region passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPeopleCitiesCountryTest()
+//    {
+//        try {
+//            ArrayList<PopulationPercent> populationPercents = app.query25();
+//            for (PopulationPercent populationPercent : populationPercents){
+//                populationPercent.countryName = "Pitcairn";
+//                populationPercent.countryPopulation = Long.valueOf(50);
+//                populationPercent.liveInCity = Long.valueOf(42);
+//                populationPercent.liveInCityPercent = (float)84.0000;
+//                populationPercent.noLiveInCity = Long.valueOf(8);
+//                populationPercent.noLiveInCityPercent = (float)16.0000;
+//            }
+//            System.out.print("Population Percent check for Country passed!");
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPopWorldTest()
+//    {
+//        try {
+//            app.query26();
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPopContinentTest()
+//    {
+//        try {
+//            ArrayList<Country> popContinent = app.query27();
+//            for (Country popCon : popContinent){
+//                popCon.Continent = "Oceania";
+//                popCon.Population = 30401150;
+//            }
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPopRegionTest()
+//    {
+//        try {
+//            ArrayList<Country> popRegion = app.query28();
+//            for (Country popReg : popRegion){
+//                popReg.Region = "Micronesia";
+//                popReg.Population = 543000;
+//            }
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPopCountryTest()
+//    {
+//        try {
+//            ArrayList<Country> popCountry = app.query29();
+//            for (Country popCou : popCountry){
+//                popCou.Name = "Pitcairn";
+//                popCou.Population = 50;
+//            }
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPopDistrictTest()
+//    {
+//        try {
+//            ArrayList<City> popDistrict = app.query30();
+//            for (City popDis : popDistrict){
+//                popDis.Name = "West Island";
+//                popDis.Population = 167;
+//            }
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
+//    @Test
+//    void getPopCityTest()
+//    {
+//        try {
+//            ArrayList<City> popCity = app.query31();
+//            for (City popCty : popCity){
+//                popCty.Name = "Adamstown";
+//                popCty.Population = 42;
+//            }
+//        } catch (Exception m) {
+//            m.printStackTrace();
+//        }
+//    }
 }
 
